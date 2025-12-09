4ec15c4abc4d5843a4be35ed3a81d89ba32229aa
Maxence G. <maxence.gatignol@supinfo.com>
2025-12-09 15:28:03 +0100

    Organisation fichiers + début fetch api

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -1,18 +1,11 @@
 const APIkey = "e3c1aaa8"
-const searchQuery = "avengers"
+const searchQuery ="Avengers"
 
-async function displayTrendingMovies() {
-    const response = await fetch(`http://www.omdbapi.com/?apikey=${APIkey}&s=${searchQuery}&type=movie`)
-    const data = await response.json()
-    const postersContainer = document.getElementById('posters');
-    if (data.Search) {
-        data.Search.forEach(movie => {
-            if (movie.Poster && movie.Poster !== 'N/A') {
-                const img = document.createElement('img');
-                img.src = movie.Poster;
-                img.alt = movie.Title;
-                img.style.width = '200px';
-                img.style.margin = '10px';
-                postersContainer.appendChild(img);
-            }
-}
+
+.fetch(`http://www.omdbapi.com/?apikey=${APIkey}&s="Avengers"&type=movie`)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error =>console.log(error))
+
+const postersContainer = document.getElementById('posters');
+
