1785a3d6a9aa38f2ebaff48ec115df23bd8e8165
Maxence G. <maxence.gatignol@supinfo.com>
2025-12-18 14:06:39 +0100

    Update trending titles in index.js

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -5,7 +5,7 @@ const postersContainer = document.getElementById('posters');
 const loadMoreBtn = document.getElementById('load-more');
 
 const TRENDING_TITLES = [
-    'The Godfather",
+    'Dune: Part Two',
     'Guardians of the Galaxy Vol. 3',
     'Avengers: Endgame',
     'Inception',
